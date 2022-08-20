6acf051bfa3a49f6eece67797836bc00c90f2870
Astewul <loloalex1992@gmail.com>
2022-08-20 11:17:25 -0300

    commiting

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -8,11 +8,11 @@
  *    */
 int infinite_while(void)
 {
-	while (1)
-		{
-			sleep(1);
-		}
-	return (0);
+while (1)
+{
+sleep(1);
+}
+return (0);
 }
 
 /**
@@ -21,15 +21,15 @@ int infinite_while(void)
  *    */
 int main(void)
 {
-	int i;
-	pid_t zombie;
-	for (i = 0; i < 5; i++)
-		{
-			zombie = fork();
-			if (!zombie)
-				return (0);
-			printf("Zombie process created, PID: %d\n", zombie);
-		}
-	infinite_while();
-	return (0);
+int i;
+pid_t zombie;
+for (i = 0; i < 5; i++)
+{
+zombie = fork();
+if (!zombie)
+return (0);
+printf("Zombie process created, PID: %d\n", zombie);
+}
+infinite_while();
+return (0);
 }
