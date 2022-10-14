2d5671611f4b344fbd86810335d4a6d342f63ec8
Astewul <loloalex1992@gmail.com>
2022-10-14 22:51:11 +0300

    commit task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # craete file in /tmp
 
-file { '/tmp/school':
+file {'/tmp/school':
   ensure => 'file',
   mode => '0744',
   owner => 'www-data',
   group => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
