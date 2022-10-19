4b658c49333ea0c91f2a095f8e302b27b266e632
Astewul <loloalex1992@gmail.com>
2022-10-19 10:20:31 +0300

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # installs the package puppet-lint
-
-package{'puppet-lint':
-    ensure   => '2.1.0',
-    provider => 'gem',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
