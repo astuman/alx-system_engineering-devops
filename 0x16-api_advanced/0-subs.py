801b9f71f0896bda2949f6b7850207b285e91bc3
Astewul <loloalex1992@gmail.com>
2022-11-29 15:10:38 +0300

    commit first file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""queries the Reddit API and return teh number of total subscribers """
+
+import requests
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/subscribers.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
+        return 0
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
