66056ca37e9e1cf5cdf00826609ed5b75d2febfe
Astewul <loloalex1992@gmail.com>
2022-11-30 18:15:46 +0300

    commit puppet code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
+# Fixes a wordpress site running on apache2
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php; sudo service apache2 restart',
-  path => ['/bin', '/usr/bin', '/usr/sbin']
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
